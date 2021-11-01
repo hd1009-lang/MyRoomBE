92ee0848a9a9e91715a0b30ef895a1a55acb62a3
Hoàng Đại <daihoangnam3it@gmail.com>
2021-11-01 16:42:15 +0700

    check

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
     fileUpload({
         useTempFiles: true,
-        tempFileDir: '/src/tmp',
+        tempFileDir: '/src',
     })
 );
 app.get('/', (req, res) => res.json({ msg: 'welcome to api' }));
